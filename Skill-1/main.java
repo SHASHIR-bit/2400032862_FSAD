e12ce3f134393266198dd121774ac7c729af6fe2
SHASHIR-bit <SHASHIR-bit@users.noreply.github.com>
2026-03-15 00:07:35 +0530

    Add Skill-1 folder

---
+++ Skill-1/main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Feature update added");
+    }
+
+    private static void bugFixMarker() {
+        System.out.println("Bug fix applied");
+    }
+}
